2863ba4e6f146272a73f1ebe96683a51b0cd2996
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2022-05-25 23:31:50 +0200

    CLI: add logging

--- lamprey-cli/__main__.py
+++ lamprey-cli/__main__.py
@@ -24,3 +24,19 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
+
+# set up logging
+#
+# -v for INFO
+# -vv for DEBUG
+log_level = max(logging.WARNING - 10 * args.verbosity, logging.DEBUG)
+assert log_level <= logging.WARNING
+assert log_level >= logging.DEBUG
+logging.basicConfig(
+    style="{",
+    format="{asctime:} {name:30} {levelname:8} {message:}",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+    level=log_level
+)
+
+logging.info("lamprey-cli PID=%d", os.getpid())
