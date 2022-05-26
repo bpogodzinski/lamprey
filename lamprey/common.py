34dd7f1e1b0d53b4ace5079f85e82426abc18ca8
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2022-05-26 19:04:46 +0200

    Add testing examples

--- lamprey/common.py
+++ lamprey/common.py
@@ -1,4 +1,6 @@
 def format_bytes(size):
+    if size < 0:
+        raise ValueError('size cannot be a negative number')
     power = 2**10
     n = 0
     power_labels = {0 : '', 1: 'KB', 2: 'MB', 3: 'GB', 4: 'TB'}
--- tests/test_common.py
+++ tests/test_common.py
@@ -1,4 +1,15 @@
+import pytest
+
 from lamprey.common import format_bytes
 
 def test_format_bytes():
-    assert format_bytes(866463744) == (826.32421875, 'MB')
+    size_1 = format_bytes(866463744)
+    assert 826.4 > size_1[0] > 826.3
+    assert size_1[1] == 'MB'
+    
+    size_2 = format_bytes(1000000000)
+    assert 953.7 > size_2[0] > 953.6
+    assert size_2[1] == 'MB'
+    
+    with pytest.raises(ValueError):
+        format_bytes(-1)
