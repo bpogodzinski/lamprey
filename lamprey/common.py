dface335f9cf09b435e2a40251501ae0259bfdfc
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2022-05-27 18:59:59 +0200

    Further cleanup code
    - minor changes in workflows

--- lamprey/common.py
+++ lamprey/common.py
@@ -2,9 +2,9 @@ def format_bytes(size):
     if size < 0:
         raise ValueError('size cannot be a negative number')
     power = 2**10
-    n = 0
+    postfix_counter = 0
     power_labels = {0 : '', 1: 'KB', 2: 'MB', 3: 'GB', 4: 'TB'}
     while size > power:
         size /= power
-        n += 1
-    return size, power_labels[n]
+        postfix_counter += 1
+    return size, power_labels[postfix_counter]
