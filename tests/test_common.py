25022fb7abcc3f0dfc24a3a69188a9e43e406753
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2022-05-27 15:44:50 +0200

    Cleanup code

--- tests/test_common.py
+++ tests/test_common.py
@@ -6,10 +6,10 @@ def test_format_bytes():
     size_1 = format_bytes(866463744)
     assert 826.4 > size_1[0] > 826.3
     assert size_1[1] == 'MB'
-    
+
     size_2 = format_bytes(1000000000)
     assert 953.7 > size_2[0] > 953.6
     assert size_2[1] == 'MB'
-    
+
     with pytest.raises(ValueError):
         format_bytes(-1)
