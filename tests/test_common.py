e6f99c20a681b0684bf50804b3538a91373da5c7
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2022-05-27 18:54:32 +0200

    Update tests

--- tests/test_common.py
+++ tests/test_common.py
@@ -3,13 +3,17 @@ import pytest
 from lamprey.common import format_bytes
 
 def test_format_bytes():
-    size_1 = format_bytes(866463744)
-    assert 826.4 > size_1[0] > 826.3
-    assert size_1[1] == 'MB'
+    size, postfix = format_bytes(866463744)
+    assert 826.4 > size > 826.3
+    assert postfix == 'MB'
 
-    size_2 = format_bytes(1000000000)
-    assert 953.7 > size_2[0] > 953.6
-    assert size_2[1] == 'MB'
+    size, postfix = format_bytes(1000000000)
+    assert 953.7 > size > 953.6
+    assert postfix == 'MB'
+
+    size, postfix = format_bytes(1)
+    assert size == 1
+    assert postfix == ''
 
     with pytest.raises(ValueError):
         format_bytes(-1)
