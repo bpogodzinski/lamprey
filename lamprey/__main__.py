8186650134c005e31c90d2d996fbf7c7f60c872b
Bartłomiej Pogodziński <28873085+bpogodzinski@users.noreply.github.com>
2022-12-01 18:00:04 +0100

    Work on sending handshake to peer

--- lamprey/__main__.py
+++ lamprey/__main__.py
@@ -1,9 +1,11 @@
+import socket
 from pprint import pprint as pp
 import argparse
 import logging
 import os
 import sys
 from datetime import datetime
+import requests
 import bencoding
 from lamprey.dataclass import Torrent
 from lamprey.tracker import Tracker
@@ -79,10 +81,42 @@ torrent_info = Torrent((torrent[b"comment"]), (torrent[b"created by"]), (datetim
     torrent[b"url-list"]), (torrent[b"info"]), (torrent[b'info'][b'name']), (torrent[b'info'][b'length']), (torrent[b'info'][b'piece length']), (torrent[b'announce']), (torrent[b'announce-list']))
 tracker = Tracker(torrent_info)
 tracker_response = tracker.connect()
-
 # 1. Zrób funkcje która przyjmuje content z odpowiedzi trackera i zwraca liste peerów
-
+peers_list = []
+offset = 6
+peers = bencoding.bdecode(tracker_response.content)[b'peers']
+if tracker_response.status_code == 200:
+    number_of_peers = len(peers)//6
+    for peer in range(0, number_of_peers):
+        peers_list.append(
+            f"{peers[0+offset+peer]}.{peers[1+offset+peer]}.{peers[2+offset+peer]}.{peers[3+offset+peer]}:{int.from_bytes(peers[4+offset+peer:6+offset+peer], byteorder='big')}")
 # 2. Połącz się z każdym peerem i wyprintuj odpowiedź od niego
+
+# The handshake is a required message and must be the first message transmitted by the client. It is (49+len(pstr)) bytes long.
+# handshake: <pstrlen><pstr><reserved><info_hash><peer_id>
+
+# Z = 19BitTorrent protocol00000000{tracker.info_hash}{tracker.peer_id}
+
+# pstrlen: string length of < pstr >, as a single raw byte
+# pstr: string identifier of the protocol
+# reserved: eight(8) reserved bytes. All current implementations use all zeroes. Each bit in these bytes can be used to change the behavior of the protocol. An email from Bram suggests that trailing bits should be used first, so that leading bits may be used to change the meaning of trailing bits.
+# info_hash: 20-byte SHA1 hash of the info key in the metainfo file. This is the same info_hash that is transmitted in tracker requests.
+# peer_id: 20-byte string used as a unique ID for the client. This is usually the same peer_id that is transmitted in tracker requests(but not always e.g. an anonymity option in Azureus).
+
+# 2.1 użyj struct do upakowania wiadomości do wysłania peerowi
+
+single_peer = peers_list[0].split(':')
+message = bytes(
+    f'19BitTorrent protocol00000000{tracker.info_hash}{tracker.peer_id}', 'utf-8')
+host = single_peer[0]
+port = single_peer[1]                   # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+s.sendall(message)
+data = s.recv(1024)
+s.close()
+print('Received', repr(data))
+
 
 if args.dry_run:
     logging.warning("dry run, won't download")
