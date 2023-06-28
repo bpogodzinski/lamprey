e10e3f5fcb34240d6027fe6299dbf233853ea1f5
Bartłomiej Pogodziński <28873085+bpogodzinski@users.noreply.github.com>
2023-06-28 16:53:20 +0200

    Co-authored-by: PrzybyszB <PrzybyszB@users.noreply.github.com>

--- lamprey/piece_menager.py
+++ lamprey/piece_menager.py
@@ -1,38 +1,53 @@
 class Block:
-    def __init__(self, size = 2**14, content = None, index = None, piece = None, is_completed = False, is_downloading = False):
+    def __init__(self, size = 2**14, content = None, piece = None, is_completed = False, is_downloading = False):
         self.size = size
         self.content = content
-        self.index = index
         self.piece = piece
         self.is_completed = is_completed
         self.is_downloading = is_downloading
 
 
 
-        self.file_begin =  size = 0
-        self.file_end = size= 2*14
-
-    
+        self.file_begin = None
+        
+        self.file_end = None
 
         self.expected_shasum = None
+    
+    def __str__(self) -> str:
+        return f'size: {self.size} piece: {self.piece.index}'
 
-blok = Block(300)
+class Piece:
+    def __init__(self, length, block_list = None, index = None, is_completed = False, is_downloading = False):
+        self.length = length
+        self.block_list = block_list if block_list else []
+        self.index = index
+        self.is_completed = is_completed
+        self.is_downloading = is_downloading
 
-print(blok.size)
+    def __str__(self) -> str:
+        return ' '.join(str(block) for block in self.block_list)
 
-# class Piece:
-#     def __init__(self, size, block_list = None, index = None, is_completed = False, is_downloading = False):
-#         self.size = size
-#         self.block_list = block_list
-#         self.index = index
-#         self.is_completed = is_completed
-#         self.is_downloading = is_downloading
 
-# # obiekt_piece = Piece(index=0)
-# # obiekt_block = Block(index=0, content='xd', piece=obiekt_piece)
+class FileManager():
+    pass
 
 # obiekt_piece = Piece(index=0)
-# obiekt_piece.block_list.append(Block(index=0))
+# obiekt_block = Block(index=0, content='xd', piece=obiekt_piece)
+
+obiekt_piece = Piece(262144, index=0)
+obiekt_piece1 = Piece(262144, index=1)
+obiekt_piece.block_list.append(Block(piece=obiekt_piece))
+obiekt_piece.block_list.append(Block(piece=obiekt_piece))
+obiekt_piece.block_list.append(Block(piece=obiekt_piece))
+obiekt_piece1.block_list.append(Block(piece=obiekt_piece1))
+obiekt_piece1.block_list.append(Block(piece=obiekt_piece1))
+obiekt_piece1.block_list.append(Block(piece=obiekt_piece1))
+
+print(str(obiekt_piece))
+print(str(obiekt_piece1))
+
+obiekt_piece.block_list[0]
 
 # obiekt_piece = Piece() # Nie zadziała
 # obiekt_piece = Piece(10) # Zadziała
